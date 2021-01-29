acef8bf50d540e631265bf51f9fd899fe543a0b1
rahel81 <75310641+rahel81@users.noreply.github.com>
2021-01-30 00:13:50 +0300

    Update RadioAdvanced.java
    Fixed RadioAdvanced

--- src/main/java/ru/netology/domain/RadioAdvanced.java
+++ src/main/java/ru/netology/domain/RadioAdvanced.java
@@ -10,15 +10,32 @@ public class RadioAdvanced {
     private int minVolume = 0;
     private boolean on;
 
+   public RadioAdvanced() {
+    }
+
+    public RadioAdvanced(int currentVolume) {
+        if (currentVolume > 100) {
+            return;
+        }
+        if (currentVolume < 0) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
     public int getCurrentNumberStation() {
         return currentNumberStation;
     }
 
     public void setCurrentNumberStation(int currentNumberStation) {
-        if (currentNumberStation > lastStation) {
+        if (currentNumberStation > 10) {
             return;
         }
-        if (currentNumberStation < startStation) {
+        if (currentNumberStation < 0) {
             return;
         }
         this.currentNumberStation = currentNumberStation;
@@ -26,75 +43,29 @@ public class RadioAdvanced {
 
     public void nextNumberStation() {
         if (currentNumberStation == lastStation) {
-            this.currentNumberStation = startStation;
-        } else {
-            currentNumberStation++;
+            setCurrentNumberStation(0);
+            return;
         }
+        currentNumberStation++;
     }
 
     public void prevNumberStation() {
         if (currentNumberStation == startStation) {
-            this.currentNumberStation = lastStation;
-        } else {
-            currentNumberStation--;
-        }
-    }
-
-    public int getLastStation() {
-        return lastStation;
-    }
-
-    public void setLastStation(int lastStation) {
-        this.lastStation = lastStation;
-    }
-
-    public int getStartStation() {
-        return startStation;
-    }
-
-    public void setStartStation(int startStation) {
-        this.startStation = startStation;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > maxVolume) {
+            setCurrentNumberStation(10);
             return;
         }
-        if (currentVolume < minVolume) {
-            return;
-        }
-        this.currentVolume = currentVolume;
+        currentNumberStation--;
     }
 
     public void plusVolume() {
-        if (currentVolume != maxVolume) {
+        if (currentVolume != 100) {
             currentVolume++;
         }
     }
 
     public void minusVolume() {
-        if (currentVolume != minVolume) {
+        if (currentVolume != 0) {
             currentVolume--;
         }
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
     }
 }
